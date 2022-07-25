9c8453ef493744c70b1cf1c2e6e7c6f369c16eb1
linguopeng <linguopeng@hyperchain.cn>
2022-07-25 21:23:14 +0800

    feat: remove data

--- src/main/java/common/GodwokenClient.java
+++ src/main/java/common/GodwokenClient.java
@@ -1,7 +1,6 @@
 package common;
 
 import contracts.GodwokenERC20;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import model.GodwokenProperties;
 import model.Web3GasProvider;
@@ -11,44 +10,44 @@ import org.web3j.protocol.http.HttpService;
 import org.web3j.tx.RawTransactionManager;
 import utils.TransactionUtil;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
-@Data
 public class GodwokenClient {
 
     private Web3j web3;
     private List<Credentials> credentialsArrays;
-    private static String DefaultPath  = "./Godwoken.properties";
+    private static String DefaultPath = "./Godwoken.properties";
     private static GodwokenClient godwokenClientInstance;
+
 
     //todo : lazy load
     private GodwokenERC20 godwokenERC20;
+
     private GodwokenClient() throws Exception {
         GodwokenProperties godwokenProperties = new GodwokenProperties(DefaultPath);
         String[] privateKeys = godwokenProperties.getPrivateKeys();
         credentialsArrays = new ArrayList<>();
-        for(String priv:privateKeys){
+        for (String priv : privateKeys) {
             Credentials credentials = Credentials.create(priv);
             credentialsArrays.add(credentials);
         }
         web3 = Web3j.build(new HttpService(godwokenProperties.getRpcUrl()));
         // if ckb address is empty deploy ckb contract
         String ckbAddress = godwokenProperties.getCkbProxyAddress();
-        RawTransactionManager rawTransactionManager = TransactionUtil.getTxManage(web3,credentialsArrays.get(0));
-        if (ckbAddress.length()<10){
-            godwokenERC20 = GodwokenERC20.deploy(web3,rawTransactionManager,new Web3GasProvider(web3)).send();
-        }else {
-            godwokenERC20 = GodwokenERC20.load(ckbAddress,web3,rawTransactionManager,new Web3GasProvider(web3));
+        RawTransactionManager rawTransactionManager = TransactionUtil.getTxManage(web3, credentialsArrays.get(0));
+        if (ckbAddress.length() < 10) {
+            godwokenERC20 = GodwokenERC20.deploy(web3, rawTransactionManager, new Web3GasProvider(web3)).send();
+        } else {
+            godwokenERC20 = GodwokenERC20.load(ckbAddress, web3, rawTransactionManager, new Web3GasProvider(web3));
         }
 
     }
 
-    public static GodwokenClient GetGodwokenInstance(){
-        if (GodwokenClient.godwokenClientInstance == null){
+    public static GodwokenClient GetGodwokenInstance() {
+        if (GodwokenClient.godwokenClientInstance == null) {
             synchronized (GodwokenClient.class) {
                 try {
                     godwokenClientInstance = new GodwokenClient();
@@ -67,27 +66,68 @@ public class GodwokenClient {
         return credentialsArrays.get(idx);
     }
 
-    public List<Credentials> getgetCredentialsArrays(){
+    public List<Credentials> getgetCredentialsArrays() {
         return credentialsArrays;
     }
 
-    public void transferCkb(Credentials credentials,String to, BigInteger value){
+    public void transferCkb(Credentials credentials, String to, BigInteger value) {
         GodwokenERC20 godwokenERC20 = getGodwokenErc20(credentials);
         try {
-            godwokenERC20.transfer(to,value).send();
+            godwokenERC20.transfer(to, value).send();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private GodwokenERC20 getGodwokenErc20(Credentials credentials){
+    private GodwokenERC20 getGodwokenErc20(Credentials credentials) {
         RawTransactionManager rawTransactionManager;
         try {
-            rawTransactionManager = TransactionUtil.getTxManage(this.web3,credentials);
+            rawTransactionManager = TransactionUtil.getTxManage(this.web3, credentials);
         } catch (Exception e) {
             e.printStackTrace();
             throw new RuntimeException("getTxManage failed ");
         }
-        return GodwokenERC20.load(this.godwokenERC20.getContractAddress(),this.web3,rawTransactionManager,new Web3GasProvider(this.web3));
+        return GodwokenERC20.load(this.godwokenERC20.getContractAddress(), this.web3, rawTransactionManager, new Web3GasProvider(this.web3));
     }
+
+    public Web3j getWeb3() {
+        return web3;
+    }
+
+    public void setWeb3(Web3j web3) {
+        this.web3 = web3;
+    }
+
+    public List<Credentials> getCredentialsArrays() {
+        return credentialsArrays;
+    }
+
+    public void setCredentialsArrays(List<Credentials> credentialsArrays) {
+        this.credentialsArrays = credentialsArrays;
+    }
+
+    public static String getDefaultPath() {
+        return DefaultPath;
+    }
+
+    public static void setDefaultPath(String defaultPath) {
+        DefaultPath = defaultPath;
+    }
+
+    public static GodwokenClient getGodwokenClientInstance() {
+        return godwokenClientInstance;
+    }
+
+    public static void setGodwokenClientInstance(GodwokenClient godwokenClientInstance) {
+        GodwokenClient.godwokenClientInstance = godwokenClientInstance;
+    }
+
+    public GodwokenERC20 getGodwokenERC20() {
+        return godwokenERC20;
+    }
+
+    public void setGodwokenERC20(GodwokenERC20 godwokenERC20) {
+        this.godwokenERC20 = godwokenERC20;
+    }
+
 }
--- src/main/java/model/GodwokenProperties.java
+++ src/main/java/model/GodwokenProperties.java
@@ -1,33 +1,30 @@
 package model;
 
 import com.google.gson.Gson;
-import lombok.Data;
-import lombok.ToString;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-@Data
-@ToString
 public class GodwokenProperties {
     private String rpcUrl;
     private String[] privateKeys;
     private String CkbProxyAddress;
     private Gson gson;
+
     public GodwokenProperties(String path) throws IOException {
         gson = new Gson();
         Properties properties = getProperties(path);
         rpcUrl = properties.getProperty("RpcUrl");
         String privateKeysString = properties.getProperty("privateKeys");
-        privateKeys = gson.fromJson(privateKeysString,String[].class);
+        privateKeys = gson.fromJson(privateKeysString, String[].class);
         CkbProxyAddress = properties.getProperty("proxyCkbAddress");
     }
 
     private Properties getProperties(String path) throws IOException {
         InputStream in;
-        if(isAbsolutePath(path)){
+        if (isAbsolutePath(path)) {
             System.out.println();
             in = new FileInputStream(path);
             Properties prop = new Properties();
@@ -42,6 +39,30 @@ public class GodwokenProperties {
 
     private static boolean isAbsolutePath(String path) {
         return path.startsWith("/") || path.startsWith("file:/") || path.contains(":\\");
+    }
+
+    public String getRpcUrl() {
+        return rpcUrl;
+    }
+
+    public void setRpcUrl(String rpcUrl) {
+        this.rpcUrl = rpcUrl;
+    }
+
+    public String[] getPrivateKeys() {
+        return privateKeys;
+    }
+
+    public void setPrivateKeys(String[] privateKeys) {
+        this.privateKeys = privateKeys;
+    }
+
+    public String getCkbProxyAddress() {
+        return CkbProxyAddress;
+    }
+
+    public void setCkbProxyAddress(String ckbProxyAddress) {
+        CkbProxyAddress = ckbProxyAddress;
     }
 
 }
